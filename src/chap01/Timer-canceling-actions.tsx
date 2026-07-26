5c1d464416e0fed97c20212d800352e35f3bdac2
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2026-07-26 15:23:29 -0400

    useEffect - canceling side-effect actions

---
+++ src/chap01/Timer-canceling-actions.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+const Timer = () => {
+  const [timer, setTimer] = React.useState(100);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => (prevTimer === 0 ? 0 : prevTimer - 1));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <p>Timer: {timer}</p>;
+};
+
+const ShowHideTimer = ({ show }) => (show ? <Timer /> : null);
+
+function App() {
+  const [show, setShow] = React.useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Hide Timer" : "Show Timer"}
+      </button>
+      <ShowHideTimer show={show} />
+    </>
+  );
+}
+
+export default App;
